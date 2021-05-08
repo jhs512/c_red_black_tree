24ba2a47380ad93f1a4218c5952e6231261463a6
jhs512 <jangka512@gmail.com>
2021-05-09 01:45:40 +0900

    일자로 뻗어있는 Black, Red, Red 패턴을 회전으로 해결

--- ConsoleApplication2/main.c
+++ ConsoleApplication2/main.c
@@ -39,6 +39,7 @@ typedef struct _RBT__Node
 
 // 루트노드
 RBT__Node* rootNode = NULL;
+RBT__Node* nilNode = NULL;
 
 RBT__Node* RBT__createNode(RBT__ElementType data) {
 	RBT__Node* node = malloc(sizeof(RBT__Node));
@@ -58,7 +59,7 @@ RBT__Node* RBT__createNode(RBT__ElementType data) {
 
 void RBT__addChildNode(RBT__Node* parentNode, RBT__Node* node) {
 	if (parentNode->data > node->data) {
-		if (parentNode->left == NULL) {
+		if (parentNode->left == nilNode) {
 			node->parent = parentNode;
 			parentNode->left = node;
 		}
@@ -67,7 +68,7 @@ void RBT__addChildNode(RBT__Node* parentNode, RBT__Node* node) {
 		}
 	}
 	else {
-		if (parentNode->right == NULL) {
+		if (parentNode->right == nilNode) {
 			node->parent = parentNode;
 			parentNode->right = node;
 		}
@@ -78,6 +79,9 @@ void RBT__addChildNode(RBT__Node* parentNode, RBT__Node* node) {
 }
 
 RBT__Node* RBT__insertNode(RBT__Node* node) {
+	node->left = nilNode;
+	node->right = nilNode;
+
 	if (rootNode == NULL) {
 		rootNode = node;
 		return node;
@@ -109,13 +113,73 @@ void RBT__printNode(RBT__Node* node, int depth, int blackNodeDepth) {
 
 	printf("[data : %04d, color : %4s, parent : %04d, parentOf : %5s, depth : %02d, blackNodeDepth:%02d]\n", node->data, node->color == BLACK ? "BLACK" : "RED", parentData, parentOf, depth, blackNodeDepth);
 
-	if ( node->left != NULL ) {
+	if ( node->left != nilNode) {
 		RBT__printNode(node->left, depth + 1, blackNodeDepth);
 	}
 
-	if (node->right != NULL) {
+	if (node->right != nilNode) {
 		RBT__printNode(node->right, depth + 1, blackNodeDepth);
 	}
+}
+
+void RBT__rotateRight(RBT__Node* parentNode) {
+	// 회전의 기준이 되는 노드
+	RBT__Node* leftChild = parentNode->left;
+
+	// 기준노드의 오른쪽 자식을 부모노드의 왼쪽자식으로
+	// 기준노드의 오른쪽 자식은 부모노드여야 하기 때문
+	parentNode->left = leftChild->right;
+
+	// 기존노드의 오른쪽 자식이 있는 경우
+	if (leftChild->right != nilNode)
+		// 기준노드의 오른쪽 자식의 부모는 더이상 기준노드가 아니라 부모노드
+		leftChild->right->parent = parentNode;
+
+	// 기준노드의 부모는 부모노드의 부모노드이다.
+	leftChild->parent = parentNode->parent;
+
+	if (parentNode->parent == NULL)
+		rootNode = leftChild;
+	else
+	{
+		if (parentNode == parentNode->parent->left)
+			parentNode->parent->left = leftChild;
+		else
+			parentNode->parent->right = leftChild;
+	}
+
+	leftChild->right = parentNode;
+	parentNode->parent = leftChild;
+}
+
+void RBT__rotateLeft(RBT__Node* parentNode) {
+	// 회전의 기준이 되는 노드
+	RBT__Node* rightChild = parentNode->right;
+
+	// 기준노드의 왼쪽 자식을 부모노드의 오른쪽자식으로
+	// 기준노드의 왼쪽 자식은 부모노드여야 하기 때문
+	parentNode->right = rightChild->left;
+
+	// 기존노드의 오른쪽 자식이 있는 경우
+	if (rightChild->left != nilNode)
+		// 기준노드의 오른쪽 자식의 부모는 더이상 기준노드가 아니라 부모노드
+		rightChild->left->parent = parentNode;
+
+	// 기준노드의 부모는 부모노드의 부모노드이다.
+	rightChild->parent = parentNode->parent;
+
+	if (parentNode->parent == NULL)
+		rootNode = rightChild;
+	else
+	{
+		if (parentNode == parentNode->parent->right)
+			parentNode->parent->right = rightChild;
+		else
+			parentNode->parent->left = rightChild;
+	}
+
+	rightChild->left = parentNode;
+	parentNode->parent = rightChild;
 }
 
 void RBT__rebuildNode(RBT__Node* node) {
@@ -125,6 +189,7 @@ void RBT__rebuildNode(RBT__Node* node) {
 		if (node->parent == node->parent->parent->left) {
 			RBT__Node* uncleNode = node->parent->parent->right;
 
+			// 삼촌도 레드인 경우, 단순
 			if (uncleNode->color == RED)
             {
                 node->parent->color = BLACK;
@@ -133,11 +198,18 @@ void RBT__rebuildNode(RBT__Node* node) {
 
                 node = node->parent->parent;
             }
+			else {
+				node->parent->color = BLACK;
+				node->parent->parent->color = RED;
+
+				RBT__rotateRight(node->parent->parent);
+			}
 		}
 		// 나의 부모가 할아버지의 오른쪽 자식인 경우
 		else {
 			RBT__Node* uncleNode = node->parent->parent->left;
 
+			// 삼촌도 레드인 경우, 단순
 			if (uncleNode->color == RED)
 			{
 				node->parent->color = BLACK;
@@ -145,6 +217,12 @@ void RBT__rebuildNode(RBT__Node* node) {
 				node->parent->parent->color = RED;
 
 				node = node->parent->parent;
+			}
+			else {
+				node->parent->color = BLACK;
+				node->parent->parent->color = RED;
+
+				RBT__rotateLeft(node->parent->parent);
 			}
 		}
 	}
@@ -176,6 +254,9 @@ void actionInsertNode() {
 
 // 메인 시작
 int main() {
+	nilNode = RBT__createNode(0);
+	nilNode->color = BLACK;
+
 	while (true) {
 		printf("== 명령어 리스트 ==\n");
 		printf("1 : 노드출력\n");
