2626033e5320f7777548d1f189b8bfccab4237e9
jhs512 <jangka512@gmail.com>
2021-05-08 23:28:53 +0900

    명령어 입력기능 보강

--- ConsoleApplication2/main.c
+++ ConsoleApplication2/main.c
@@ -4,15 +4,35 @@ TODO LIST
 */
 
 #include <stdio.h>
+#include<ctype.h>
+#include<string.h>
+
 #define true 1
 #define false 0
 
-int main() {
-	char command[21];
+char command[21];
 
+void inputCommand() {
+	printf("명령어) ");
+	fgets(command, sizeof command, stdin);
+
+	for (int i = 0; true; i++) {
+		if (command[i] == NULL && i > 0) {
+			command[i - 1] = NULL;
+			break;
+		}
+	}
+}
+
+int main() {
 	while (true) {
-		printf("명령어) ");
-		scanf_s("%s", command, sizeof(command));
+		printf("== 명령어 리스트 ==\n");
+		printf("1 : 노드출력\n");
+		printf("2 : 노드추가\n");
+		printf("3 : 노드삭제\n");
+		printf("4 : 노드검색\n");
+
+		inputCommand();
 
 		printf("%s\n", command);
 	}
